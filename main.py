e38840e8b8300f7302699225f6629f41e6617d9a
jorgejcabrera <cabrerajjorge@gmail.com>
2022-04-05 22:00:45 -0300

    taking kilometers parameter by console

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
+import sys
+
 from find_providers_to_hire import FindProvidersToHireUseCase
 from provider_reader import ProviderReader
 
 if __name__ == '__main__':
-    reader = ProviderReader("contratos.txt")
+
+    try:
+        file_path = sys.argv[1]
+        kilometers = sys.argv[2]
+    except:
+        print("Taking default values")
+        file_path = "contratos.txt"
+        kilometers = 200
+
+    reader = ProviderReader(file_path)
     providers = reader.read()
     use_case = FindProvidersToHireUseCase()
-    providers_to_hire = use_case.invoke(providers, 500)
+    providers_to_hire = use_case.invoke(providers, int(kilometers))
     print(providers_to_hire)
