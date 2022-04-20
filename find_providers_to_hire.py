e638810ca1c7491a4fb105739e6af393d72aab83
jorgejcabrera <cabrerajjorge@gmail.com>
2022-04-20 16:59:05 -0300

    measuting the performance of a new v2 greedy implementation

--- find_providers_to_hire.py
+++ find_providers_to_hire.py
@@ -18,3 +18,18 @@ class FindProvidersToHireUseCase:
             providers_to_hire.append(new_provider)
             coverage = new_provider.higher_position()
         return providers_to_hire
+
+    def invoke_v2(self, providers, target_position):
+        sorted_providers = self.service.sort(providers)
+        coverage = 0
+        providers_to_hire = []
+
+        while coverage < target_position and len(sorted_providers) > 0:
+            best_provider = sorted_providers.pop(0)
+            while len(sorted_providers) > 0 and sorted_providers[0].lower_position() <= coverage:
+                next_provider = sorted_providers.pop(0)
+                if next_provider.higher_position() >= best_provider.higher_position():
+                    best_provider = next_provider
+            coverage = best_provider.higher_position()
+            providers_to_hire.append(best_provider)
+        return providers_to_hire
--- test/test_performance_report.py
+++ test/test_performance_report.py
@@ -19,19 +19,27 @@ class TestPerformanceReport(TestCase):
         amount_of_providers = 10
         kilometers = 100000
         x = []
-        y = []
+        y_v1 = []
+        y_v2 = []
         while amount_of_providers < kilometers:
             providers = sample_of(amount_of_providers, kilometers)
             started_time = current_milli_time()
             find_providers_to_hire.invoke_v1(providers, kilometers)
             end_time = current_milli_time() - started_time
 
+            started_time_v2 = current_milli_time()
+            find_providers_to_hire.invoke_v2(providers, kilometers)
+            end_time_v2 = current_milli_time() - started_time_v2
+
             x.append(amount_of_providers)
-            y.append(end_time)
+            y_v1.append(end_time)
+            y_v2.append(end_time_v2)
 
             amount_of_providers += 500
 
-        plt.plot(x, y)
+        plt.plot(x, y_v1)
+        plt.plot(x, y_v2)
         # function to show the plot
         plt.show()
+
         self.assertTrue(1 == 1)
